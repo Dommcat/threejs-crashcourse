8659d2638ff47debb1493324ae8a5bd1b3346af9
Dominick Martin <dominickbmartin@gmail.com>
2023-05-22 19:07:05 +0000

    Square Ball

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,56 @@
+import * as THREE from 'three'; 
+
+//Scene 
+const scene = new THREE.Scene();
+
+//Create our sphere 
+const geometry = new THREE.SphereGeometry (3, 64, 64)
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83",
+})
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+//Light
+const light = new THREE.PointLight(0xffffff, 1, 100)
+light.position.set(0, 10 , 10)
+scene.add(light)
+
+//Camera 
+const camera = new THREE.PerspectiveCamera(45, 800 / 600)
+camera.position.z = 20 
+scene.add(camera)
+
+//Renderer
+const canvas = document.querySelector(".webgl")
+const renderer = new THREE.WebGLRenderer({ canvas })   
+renderer.setSize(800, 600)
+renderer.render(scene, camera)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
